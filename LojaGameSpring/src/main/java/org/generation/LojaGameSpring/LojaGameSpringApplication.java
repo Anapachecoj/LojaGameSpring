58e7bdfc64df42f0f9e1f41e5521208b7f895b51
Anapachecoj <anadejesusecarol@gmail.com>
2021-07-31 20:19:18 -0300

    Exercico Game

---
+++ LojaGameSpring/src/main/java/org/generation/LojaGameSpring/LojaGameSpringApplication.java
@@ -0,0 +1,13 @@
+package org.generation.LojaGameSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaGameSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaGameSpringApplication.class, args);
+	}
+
+}
---
+++ LojaGameSpring/src/test/java/org/generation/LojaGameSpring/LojaGameSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.LojaGameSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaGameSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
